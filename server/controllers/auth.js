399b337c3b19796662d0e1be235f0e4292b46155
chazmcgrill <charlietaylorcoder@gmail.com>
2018-11-26 20:49:24 +0000

    Adds Auth Controller User DB Logic

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,29 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
     // check db for email
-    // check for duplicate email
-    // if email unique create user save record
-    // confirm creation
-
-    res.send({ email, password })
+    User.findOne({ email: email }, function(err, existingUser) {
+        if (err) return next(err);
+        if (!email || !password) {
+            return res.status(422).send({
+                error: 'Email and password are required.'
+            });
+        }
+        // check for duplicate email
+        if (existingUser) {
+            return res.status(422).send({
+                error: 'Email already in use.'
+            });
+        }
+        // if email unique create user save record
+        const user = new User({ email: email, password: password });
+        user.save(function(err) {
+            if (err) return next(err);
+            // confirm creation
+            res.send({ success: 'user created' });
+        })
+    });
 }
